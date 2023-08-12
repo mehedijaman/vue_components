6806c516d3871e04f478a5914b22fcd640db2a1c
Mehedi Jaman <mail4mjaman@gmail.com>
2023-08-12 12:32:57 +0600

    Episode-02 assignment done

---
+++ src/data/data.js
@@ -0,0 +1,36 @@
+const data = [
+    {
+        name: 'The Magnificent Bogra',
+        description:'Located in Rajshahi Division, Bogra is one of the oldest and most fascinating towns in Bangladesh ',
+        distance: '196 km',
+        stars: 5,
+        reviews:[
+            {
+                reviwer:'John Doe',
+                review:'Nice place to visit'
+            }
+        ],
+        places:[
+            'Mahasthangarh','Vasu Bihar','Momo Inn'
+        ],
+        photo:'https://i.ibb.co/Kr4b0zJ/152013403-10158311889099633-8423107287930246533-o.jpg'
+    },
+    {
+        name: 'The Magnificent Bogra',
+        description:'Located in Rajshahi Division, Bogra is one of the oldest and most fascinating towns in Bangladesh ',
+        distance: '196 km',
+        stars: 5,
+        reviews:[
+            {
+                reviwer:'John Doe',
+                review:'Nice place to visit'
+            }
+        ],
+        places:[
+            'Mahasthangarh','Vasu Bihar','Momo Inn'
+        ],
+        photo:'https://i.ibb.co/Kr4b0zJ/152013403-10158311889099633-8423107287930246533-o.jpg'
+    }
+]
+
+export default data
